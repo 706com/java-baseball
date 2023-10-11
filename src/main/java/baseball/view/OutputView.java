7db4aef616ed2ab608ccbe8bffbb681bdcd1eeba
706com <ehdgusjdbswl@gmail.com>
2023-10-11 22:37:33 +0900

    feat : 입출력을 담당하는 view 생성

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class OutputView {
+    private final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+
+    public void printGameStartMessage(){
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
