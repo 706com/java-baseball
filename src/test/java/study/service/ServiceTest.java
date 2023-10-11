f85207790fddb5aace425911dfd60924a1c5b0f9
706com <ehdgusjdbswl@gmail.com>
2023-10-12 02:53:38 +0900

    test : 숫자 제시 메서드 테스트(정상,예외) 구현

---
+++ src/test/java/study/service/ServiceTest.java
@@ -0,0 +1,48 @@
+package study.service;
+
+import baseball.domain.Baseball;
+import baseball.domain.Computer;
+import baseball.domain.User;
+import baseball.service.BaseballService;
+import static org.junit.jupiter.api.Assertions.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.mockito.Mockito.*;
+
+public class ServiceTest {
+
+    private BaseballService baseballService = new BaseballService();
+
+    @Test
+    @DisplayName("숫자 3개가 들어왔을 때 (정상)")
+    public void callNumber_정상(){
+
+        String input = "123";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Baseball baseball = new Baseball(new User(),new Computer());
+
+        assertDoesNotThrow(() -> baseballService.callNumber(baseball));
+    }
+
+    @Test
+    public void callNumber_예외(){
+
+        String input = "1234";
+
+        Baseball mockBaseball = mock(Baseball.class);
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThrows(IllegalArgumentException.class, () -> baseballService.callNumber(mockBaseball));
+    }
+
+}
