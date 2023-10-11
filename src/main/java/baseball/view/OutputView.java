c9c40ff2a44021dd9b5e170c5df7b25703a9f8cf
706com <ehdgusjdbswl@gmail.com>
2023-10-12 01:15:58 +0900

    feat : 게임종료 안내문 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,8 +5,13 @@ import camp.nextstep.edu.missionutils.Console;
 public class OutputView {
     private final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
+    private final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public void printGameStartMessage(){
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public void printGameOverMessage(){
+        System.out.println(GAME_OVER_MESSAGE);
     }
 }
